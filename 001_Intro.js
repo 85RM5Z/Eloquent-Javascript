02d6442b754de6fcfae4723e4c4b5e4bc7466ea3
Chamika THALAGALAGE <chamika_thalagalage@hotmail.fr>
2020-11-15 23:41:12 +0100

    initial commit

---
+++ 001_Intro.js
@@ -0,0 +1,50 @@
+/*		“We think we are creating the system for our own purposes. We
+		believe we are making it in our own image... But the computer is
+		not really like us. It is a projection of a very slim part of ourselves:
+		that portion devoted to logic, order, rule, and clarity.”
+							—Ellen Ullman, Close to the Machine: Technophilia and its Discontents*/
+
+
+// Programming is the act of constructing a program—a set of precise instructions telling a computer what to do.
+
+// A programming language is an artificially constructed language used to instruct computers.
+
+
+
+/*ON PROGRAMMING*/
+
+	/* 
+		A program is many things. It is a piece of text typed by a programmer,it 
+		is the directing force that makes the computer do what it does, it is data
+		in the computer’s memory, yet it controls the actions performed on this same
+		memory.
+	*/
+
+	/* 
+		The art of programming is the skill of controlling complexity. 
+		The great program is subdued—made simple in its complexity.
+	*/
+
+
+/*WHY LANGUAGE MATTERS*/
+
+	/*
+	    A good programming language helps the programmer by allowing them to
+		talk about the actions that the computer has to perform on a higher level.
+
+	*/
+
+/*WHAT IS JAVASCRIPT*/
+
+	/*	JavaScript is a scripting or programming language that allows you to implement complex features on web pages 
+		— every time a web page does more than just sit there and display static information for you to look at 
+		— displaying timely content updates, interactive maps, animated 2D/3D graphics, scrolling video jukeboxes, etc. 
+		— you can bet that JavaScript is probably involved.
+	*/
+
+
+/*CODE AND WHAT TO DO WITH IT*/
+
+	/*
+		Code is the text that makes up programs.
+	*/
