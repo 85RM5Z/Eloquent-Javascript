2d821f1216230ca813bf8ba9d7f21d2c2d286e25
Chamika THALAGALAGE <chamika_thalagalage@hotmail.fr>
2020-11-18 10:01:53 +0100

    Update 004_Functions.js

--- 004_Functions.js
+++ 004_Functions.js
@@ -25,7 +25,7 @@
 	/* PROBLEMATIC :Define a recursive function isEven corresponding to this description. 
 									The function should accept a single parameter (a positive, whole number) and return
 									a Boolean.
-									Test it on 50 and 75. See how it behaves on -1. 	*/
+									Test it on 50 and 75. See how it behaves on -1. */
 
 	// Author's Solution :
 
