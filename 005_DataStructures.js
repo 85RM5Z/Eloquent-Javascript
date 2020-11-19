5bc372ccacb3aa88febd7ffeaadd761deb054168
Chamika THALAGALAGE <chamika_thalagalage@hotmail.fr>
2020-11-19 18:40:44 +0100

    Update 005_DataStructures.js
    adjustments on code and comment alignment

--- 005_DataStructures.js
+++ 005_DataStructures.js
@@ -3,16 +3,16 @@
 	// The sum of a range: 
 
 	/* PROBLEMATIC : 	Write a range function that takes two arguments, start and end , and returns
-						an array containing all the numbers from start up to (and including) end .
-						Next, write a sum function that takes an array of numbers and returns the
-						sum of these numbers. Run the example program and see whether it does
-						indeed return 55.
-						As a bonus assignment, modify your range function to take an optional third
-						argument that indicates the “step” value used when building the array. If no
-						step is given, the elements go up by increments of one, corresponding to the
-						old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7,9]. 
-						Make sure it also works with negative step values so that range(5, 2, -1)
-						produces [5, 4, 3, 2] .		*/
+				an array containing all the numbers from start up to (and including) end .
+				Next, write a sum function that takes an array of numbers and returns the
+				sum of these numbers. Run the example program and see whether it does
+				indeed return 55.
+				As a bonus assignment, modify your range function to take an optional third
+				argument that indicates the “step” value used when building the array. If no
+				step is given, the elements go up by increments of one, corresponding to the
+				old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7,9]. 
+				Make sure it also works with negative step values so that range(5, 2, -1)
+				produces [5, 4, 3, 2] .		*/
 
 	// Author's Solution :
 
@@ -20,9 +20,9 @@
 	  		let array = [];
 
 	  		if (step > 0) {
-	    		for (let i = start; i <= end; i += step) array.push(i);
-	  		} else {
-	    		for (let i = start; i >= end; i += step) array.push(i);
+	    			for (let i = start; i <= end; i += step) array.push(i);
+	  			} else {
+	    			for (let i = start; i >= end; i += step) array.push(i);
 	  		}
 	  		return array;
 		}
@@ -30,7 +30,7 @@
 		function sum(array) {
 	  		let total = 0;
 	  		for (let value of array) { // Here we are looping through values not index, so 'value' is the actual value inside the array  
-	    		total += value;
+	    			total += value;
 	  		}
 	  		return total;
 		}
@@ -51,10 +51,10 @@
 		// The start and end arguments looks normal but step is pretty bizarre,
 		step = start < end ? 1 : -1 
 		step =  // step equals "something", this means that if when the function range is called 
-				// and the step arg doesn't get a value assigned to it, step will be equal to "something" by default
+			// and the step arg doesn't get a value assigned to it, step will be equal to "something" by default
 				 	
 		start < end ? 1 : -1 // This is a simple if condition with the ternary operator ?
-							 // It says : if (start < end) step gets assigned the value 1 or else -1
+		// It says : if (start < end) step gets assigned the value 1 or else -1
 		
 
 			let array = []; // The variable array is initilized to an empty array.
@@ -76,11 +76,11 @@
 	    // Reversing an array
 
 	    /* PROBLEMATIC : 	Write two functions, reverseArray and reverseArrayInPlace.
-	    					The first, reverseArray , takes an array as argument
-							and produces a new array that has the same elements in the inverse order. The
-							second, reverseArrayInPlace , does what the reverse method does: it modifies
-							the array given as argument by reversing its elements. Neither may use the
-							standard reverse method.	*/
+	    			The first, reverseArray , takes an array as argument
+				and produces a new array that has the same elements in the inverse order. The
+				second, reverseArrayInPlace , does what the reverse method does: it modifies
+				the array given as argument by reversing its elements. Neither may use the
+				standard reverse method.	*/
 
 
 		// Author's Solution :
@@ -88,16 +88,16 @@
 			function reverseArray(array) {
   				let output = [];
   				for (let i = array.length - 1; i >= 0; i--) {
-    				output.push(array[i]);
+    					output.push(array[i]);
   				}
   				return output;
 			}
 
 			function reverseArrayInPlace(array) {
   				for (let i = 0; i < Math.floor(array.length / 2); i++) {
-    				let old = array[i];
-    				array[i] = array[array.length - 1 - i];
-    				array[array.length - 1 - i] = old;
+    					let old = array[i];
+    					array[i] = array[array.length - 1 - i];
+    					array[array.length - 1 - i] = old;
   				}
   				return array;
 			}
@@ -118,17 +118,17 @@
   				for (let i = array.length - 1; i >= 0; i--) {
   					let i // declares a variable i in the for loop
   					array.length - 1 // i is assigned to array.length - 1, because we want to start looping 
-  									 // from the last element to the first element of the array. 
+  							// from the last element to the first element of the array. 
   					array.length // returns the length of the array 
-  								 // Ex: array=[a,b,c] -> array.length -> 3
-  								 // But we know that the index of an array starts at 0
-  								 // So if we give i just the value of array.length, let i would be equal to 3,
-  								 // in our example array[3] doesn't exist.  
-  								 // In order to get the last element we add -1 to the length of the array.
+							// Ex: array=[a,b,c] -> array.length -> 3
+							// But we know that the index of an array starts at 0
+							// So if we give i just the value of array.length, let i would be equal to 3,
+							// in our example array[3] doesn't exist.  
+							// In order to get the last element we add -1 to the length of the array.
   					i >= 0  // When we are reverse looping an array we are going from the highest index to the lowest index.
-  							// [a,b,c]
-  							//  0 1 2 : from the highest index 2 to the lowest index 0 
-  					i--  // Until the condtion (i >= 0) is not verified the index will be decrementing 
+						// [a,b,c]
+						//  0 1 2 : from the highest index 2 to the lowest index 0 
+  					i--	// Until the condtion (i >= 0) is not verified the index will be decrementing 
   					output.push(array[i]) // The element in position i of array will be added/pushed into the array named output.
   				}
   				return output; // return the newly reversed array output.
@@ -169,20 +169,20 @@
 		// A list
 
 		/* PROBLEMATIC : Write a function arrayToList that builds up a list structure like the one
-						 shown when given [1, 2, 3] as argument. Also write a listToArray function
-						 that produces an array from a list. Then add a helper function prepend , which
-						 takes an element and a list and creates a new list that adds the element to the
-						 front of the input list, and nth , which takes a list and a number and returns
-						 the element at the given position in the list (with zero referring to the first
-						 element) or undefined when there is no such element.
-						 If you haven’t already, also write a recursive version of nth . */
+				shown when given [1, 2, 3] as argument. Also write a listToArray function
+				that produces an array from a list. Then add a helper function prepend , which
+				takes an element and a list and creates a new list that adds the element to the
+				front of the input list, and nth , which takes a list and a number and returns
+				the element at the given position in the list (with zero referring to the first
+				element) or undefined when there is no such element.
+				If you haven’t already, also write a recursive version of nth . */
 
 		// Author's Solution :
 
 			function arrayToList(array) {
   				let list = null;
   				for (let i = array.length - 1; i >= 0; i--) {
-    				list = {value: array[i], rest: list};
+    					list = {value: array[i], rest: list};
   				}
   				return list;
 			}
@@ -190,7 +190,7 @@
 			function listToArray(list) {
   				let array = [];
   				for (let node = list; node; node = node.rest) {
-    				array.push(node.value);
+    					array.push(node.value);
   				}
   				return array;
 			}
@@ -235,8 +235,8 @@
     				list = {
     					// the value of the current element
     					value: array[i], // value = 20, because of the reverse iteration.
-    					rest: list 		 // rest = null, because rest points to the previous value, 
-    									 // (which is initilized to null when we first declared the list) 
+    					rest: list 	// rest = null, because rest points to the previous value, 
+    							// (which is initilized to null when we first declared the list) 
     				};
     				// now it is assigned. list = {value: 20, rest: null}
 
@@ -246,8 +246,8 @@
     				list = {
         				//the value of the current element
 						value: array[i], // value = 10  								
-						rest: list 		 // rest = {value: 20, rest: null}, because rest is no longer empty
-										 // rest points to the previous value we created on the 1st iteration.
+						rest: list 	// rest = {value: 20, rest: null}, because rest is no longer empty
+								// rest points to the previous value we created on the 1st iteration.
 					};
 					// now it is assigned. list = {value: 10, rest: {value: 20, rest: null}}
 
@@ -256,9 +256,9 @@
 					// { 
 					//	 value : 10 
 					//	 rest : -----------> {
-					//							value : 20
-					//							rest : null
-					//						 }
+					//				value : 20
+					//				rest : null
+					//			 }
 					// }
 				}
 
@@ -271,7 +271,7 @@
 			// trasform it into an array and return the array.
 			function listToArray(list) {
 				let array = [];
-					// declare a empty array to be used as the resulting array
+				// declare a empty array to be used as the resulting array
 
 				for (let node = list; node; node = node.rest) {
 
@@ -303,19 +303,19 @@
 
 			// Take a list and an index, return the value at said index
 			function nth(list, n) {
-  				if (!list) return undefined;// checks if n is in the list list.
-  											// in other word checks if the nth index is accessible from the list
-  											// Ex: console.log(nth(arrayToList([10, 20, 30]), 1));
-  											// So here we are using the arrayToList function to transform the array [10,20,30] into a list.
-  											// And searches to console.log the nth value/index(in our case we are trying get the value of the elem at index 1) of array->list.
-  											// Which returns -> 20
-  											// More examples: 
-  											// console.log(nth(arrayToList([10, 20, 30]), 0));
-  											// returns -> 10
-  											// console.log(nth(arrayToList([10, 20, 30]), 2));
-  											// returns -> 30
-  											// console.log(nth(arrayToList([10, 20, 30]), 3));
-											// returns undefined, because the 3rd index is not accessible
+  				if (!list) return undefined;	// checks if n is in the list list.
+  								// in other word checks if the nth index is accessible from the list
+  								// Ex: console.log(nth(arrayToList([10, 20, 30]), 1));
+  								// So here we are using the arrayToList function to transform the array [10,20,30] into a list.
+  								// And searches to console.log the nth value/index(in our case we are trying get the value of the elem at index 1) of array->list.
+  								// Which returns -> 20
+  								// More examples: 
+  								// console.log(nth(arrayToList([10, 20, 30]), 0));
+  								// returns -> 10
+  								// console.log(nth(arrayToList([10, 20, 30]), 2));
+  								// returns -> 30
+  								// console.log(nth(arrayToList([10, 20, 30]), 3));
+								// returns undefined, because the 3rd index is not accessible
 
   				else if (n == 0) return list.value; 
 					// returns the first element of the list
@@ -336,8 +336,8 @@
 			// Deep comparison
 
 			/* PROBLEMATIC : Write a function deepEqual that takes two values and returns true only if they
-							 are the same value or are objects with the same properties, where the values
-							 of the properties are equal when compared with a recursive call to deepEqual .  */
+					are the same value or are objects with the same properties, where the values
+					of the properties are equal when compared with a recursive call to deepEqual .  */
 
 			// Author's Solution :
 
